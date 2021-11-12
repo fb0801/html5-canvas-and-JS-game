72970143ac1a446a021ae63abefc2f8f60c9c369
fb0801 <fbhatti0801@gmail.com>
2021-11-12 21:35:10 +0000

    projectiles

--- index.js
+++ index.js
@@ -21,6 +21,23 @@ class Player{
             c.fill()
     }
 }
+class Projectile{
+    constructor(x,y, radius, color, velocity){
+        this.x=x
+        this.y=y
+        this.radius= radius
+        this.color = color
+        this.velocity = velocity
+    }
+    draw(){
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 
+            0, Math.PI * 2, false)
+            c.fillStyle = this.color
+            c.fill()
+    }
+}
+
 
 const x = canvas.width/2
 
@@ -28,3 +45,14 @@ const y = canvas.height/2
 
 const player = new Player(x, y, 30, 'blue') //cords of object
 player.draw()
+
+window.addEventListener('click', (event) =>{
+    const projectile = new Projectile(
+        event.clientX, 
+        event.clientY, 
+        5, 
+        'red', 
+        null
+    )    
+    projectile.draw()
+})
