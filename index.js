109d42ab1184982d1bc99d90c82b5c6db4d21377
fb0801 <fbhatti0801@gmail.com>
2021-12-01 21:51:09 +0000

    enemies 2

--- index.js
+++ index.js
@@ -43,6 +43,29 @@ class Projectile{
     }
 }
 
+class Projectile{
+    constructor(x,y, radius, color, velocity){
+        this.x=x
+        this.y=y
+        this.radius= radius
+        this.color = color
+        this.velocity = velocity
+    }
+    draw(){
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 
+            0, Math.PI * 2, false)
+            c.fillStyle = this.color
+            c.fill()
+    }
+    update(){
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
+
+
 
 const x = canvas.width/2
 
@@ -73,6 +96,24 @@ const projectile2 = new Projectile(
 )
     const projectiles = [] //hold projectiles 38.35
 
+function spawnEnemies(){
+    setInterval() => {
+        const x
+        const y
+        const radius
+        const color = 'green'
+        const angle = Math.atan2(
+            Y - canvas.width / 2,
+            event.clientX - canvas.width / 2)
+        const velocity = {
+            x:Math.cos(angle),
+            y:Math.sin(angle)
+        }
+
+        enemies.push(new Enemy(x,y,radius,color, velocity))
+    }, 1000
+}
+
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0,0, canvas.width, canvas.height)//draw the bullets anc clr screen
@@ -80,6 +121,9 @@ function animate() {
 
     projectiles.foreach(Projectile => {
         Projectile.update()
+    })
+    enemies.foreach(enemy => {
+        enemy.update()
     })
 }
 
